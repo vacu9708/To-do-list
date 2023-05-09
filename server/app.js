e901b040b0207e6b32ef7e8798833d9b29841fb8
Youngsik Yang <vacu9708@gmail.com>
2023-05-10 00:45:16 +0900

    CI/CD test

--- server/app.js
+++ server/app.js
@@ -27,5 +27,5 @@ const port = process.env.PORT || 4000;
 app.listen(port, (err) => {
   if (err) 
     return console.log(err);
-  console.log('Server running on port2: ', port);
+  console.log('Server running on port: ', port);
 });
